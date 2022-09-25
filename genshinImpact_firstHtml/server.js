7ce0f360169cf85f7b6748765906080c14c37ec8
Kiabus <59321179+Kiabus@users.noreply.github.com>
2022-09-25 21:16:23 +0200

    Add files via upload

--- genshinImpact_firstHtml/server.js
+++ genshinImpact_firstHtml/server.js
@@ -12,6 +12,29 @@ const app = express();
 
 //database
 const weapons = [
+    {id: 1, name: 'Mistsplitter Reforged', typeId: 1},
+    {id: 2, name: 'Thundering Pulse', typeId: 2},
+    {id: 3, name: 'The Widsith', typeId: 3},
+    {id: 4, name: 'Redhorn Stonethresher', typeId: 4},
+    {id: 5, name: 'Favonious Lance', typeId: 5}
+]
+
+const characters = [
+    {id: 1, name: 'Fischl', weaponId: 2, typeId: [1, 2, 3]},
+    {id: 2, name: 'Noelle', weaponId: 4, typeId: [1, 2, 3, 4]},
+    {id: 3, name: 'Yanfei', weaponId: 3, typeId: [1, 2]},
+    {id: 4, name: 'Rosaria', weaponId: 5, typeId: [1, 3]},
+    {id: 5, name: 'Kuki', weaponId: 1, typeId: [4]}
+]
+
+const charTypes = [
+    {id: 1, name: 'Main DPS'},
+    {id: 2, name: 'Sub DPS'},
+    {id: 3, name: 'Support'},
+    {id: 4, name: 'Healer'}
+]
+
+const weaponTypes = [
     {id: 1, name: 'Sword'},
     {id: 2, name: 'Bow'},
     {id: 3, name: 'Catalyst'},
@@ -19,24 +42,23 @@ const weapons = [
     {id: 5, name: 'Polearm'}
 ]
 
-const characters = [
-    {id: 1, name: 'Fischl', weaponId: 2},
-    {id: 2, name: 'Noelle', weaponId: 4},
-    {id: 3, name: 'Yanfei', weaponId: 3},
-    {id: 4, name: 'Rosaria', weaponId: 5},
-    {id: 5, name: 'Kuki', weaponId: 1}
-]
-
 //creo schema di un character
-const CharType = new GraphQLObjectType({
+const Character = new GraphQLObjectType({
     name: 'Character',
     description: 'This represent a Genshin Impact Character',
     fields: () => ({
         id: {type: new GraphQLNonNull(GraphQLInt)},
         name: {type: new GraphQLNonNull(GraphQLString)},
+        /*typeId: {type: new GraphQLNonNull(GraphQLInt)},
+        charType: {
+            type: CharType,
+            resolve: (character) => {
+                return charTypes.find(charType => charType.id === character.typeId)
+            }
+        },*/
         weaponId: {type: new GraphQLNonNull(GraphQLInt)},
         weapon: {
-            type: WeaponType,
+            type: Weapon,
             resolve: (character) => {
                 return weapons.find(weapon => weapon.id === character.weaponId)
             }
@@ -45,18 +67,50 @@ const CharType = new GraphQLObjectType({
 })
 
 //creo schema di una weapon
-const WeaponType = new GraphQLObjectType({
+const Weapon = new GraphQLObjectType({
     name: 'Weapon',
     description: 'This represent a Genshin Impact Weapon',
     fields: () => ({
         id: {type: new GraphQLNonNull(GraphQLInt)},
         name: {type: new GraphQLNonNull(GraphQLString)},
         character: {
-            type: new GraphQLList(CharType),
+            type: new GraphQLList(Character),
             resolve: (weapon) => {
             return characters.filter(character => character.weaponId === weapon.id)
             }
         }  
+    })
+})
+
+//creo schema di un tipo character
+const CharacterType = new GraphQLObjectType({
+    name: 'CharacterType',
+    description: 'This represent a Genshin Impact character type',
+    fields: () => ({
+        id: {type: new GraphQLNonNull(GraphQLInt)},
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        character: {
+            type: new GraphQLList(Character),
+            resolve: (charType) => {
+                return characters.filter(character => character.typeId === charType.id)
+            }
+        }
+    })
+})
+
+//creo schema di un tipo weapon
+const WeaponType = new GraphQLObjectType({
+    name: 'WeaponType',
+    description: 'This represent a Genshin Impact weapon type',
+    fields: () => ({
+        id: {type: new GraphQLNonNull(GraphQLInt)},
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        weapon: {
+            type: new GraphQLList(Weapon),
+            resolve: (weaponType) => {
+                return weapons.filter(weapon => weapon.typeId === weaponType.id)
+            }
+        }
     })
 })
 
@@ -66,7 +120,7 @@ const RootQueryType = new GraphQLObjectType({
     description: 'Root Query',
     fields: () => ({
         character: { //one character
-            type: CharType,
+            type: Character,
             description: 'A character of Genshin Impact',
             args: {
                 id: { type: GraphQLInt }
@@ -74,12 +128,17 @@ const RootQueryType = new GraphQLObjectType({
             resolve: (parent, args) => characters.find(character => character.id === args.id)
         },
         characters: { //all characters
-            type: new GraphQLList(CharType),
+            type: new GraphQLList(Character),
             description: 'List of All Genshin Characters',
             resolve: () => characters
         },
+        charTypes: { //all character types
+            type: new GraphQLList(CharacterType),
+            description: 'List of character types',
+            resolve: () => charTypes
+        },
         weapon: { //one weapon
-            type: WeaponType,
+            type: Weapon,
             description: 'A weapon of Genshin Impact',
             args: {
                 id: { type: GraphQLInt }
@@ -87,9 +146,14 @@ const RootQueryType = new GraphQLObjectType({
             resolve: (parent, args) => weapons.find(weapon => weapon.id === args.id)
         },
         weapons: { //all weapons
-            type: new GraphQLList(WeaponType),
+            type: new GraphQLList(Weapon),
             description: 'List of All Genshin Weapons',
             resolve: () => weapons
+        },
+        weaponTypes: { //all weapon types
+            type: new GraphQLList(WeaponType),
+            description: 'List of weapon types',
+            resolve: () => weaponTypes
         }
     })
 })
@@ -100,7 +164,7 @@ const RootMutationType = new GraphQLObjectType({
     description: 'Root Mutation',
     fields: () => ({
         addCharacter: {
-            type: CharType,
+            type: Character,
             description: 'Add a character',
             args: {
                 name: { type: new GraphQLNonNull(GraphQLString) },
@@ -113,7 +177,7 @@ const RootMutationType = new GraphQLObjectType({
             }
         },
         addWeapon: {
-            type: WeaponType,
+            type: Weapon,
             description: 'Add a weapon',
             args: {
                 name: { type: new GraphQLNonNull(GraphQLString) }
