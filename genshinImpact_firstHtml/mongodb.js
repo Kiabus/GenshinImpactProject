714119a42a6522335dafb87c392319f3dfd0d100
Kiabus <59321179+Kiabus@users.noreply.github.com>
2023-01-03 12:17:05 +0100

    add Genshin API with first GET to characters and weapons

---
+++ genshinImpact_firstHtml/mongodb.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const DBUrl = "mongodb+srv://kiabus:LoScr1c1o@genshinimpact.jxjnt.mongodb.net/?retryWrites=true&w=majority";
+const MongoClient = require('mongodb').MongoClient
+const app = express()
+
+app.use(express.json())
+
+var database 
+
+//GET
+app.get('/', (req, resp) => {
+    resp.send('Welcome to Genshin API')
+})
+
+app.get('/api/characters', (req, resp) => {
+    database.collection('characters').find({}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+})
+
+app.get('/api/characters/Fischl', (req, resp) => {
+    database.collection('characters').find({name: "Fischl"}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+
+})
+
+app.get('/api/weapons', (req, resp) => {
+    database.collection('weapons').find({}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+})
+
+app.get('/api/weapons/HuntersBow', (req, resp) => {
+    database.collection('weapons').find({name: "Hunter's Bow"}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+})
+
+//mongodb connection
+app.listen(8080, () => {
+    MongoClient.connect(DBUrl, {useNewUrlParser: true}, (error, result) =>{
+        if(error) throw error
+        database = result.db('genshin')
+        console.log('Connection successful!')
+    })
+})
