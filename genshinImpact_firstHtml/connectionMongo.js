10973f6a4e098bd1ef14d889642dd1fad018729c
Kiabus <59321179+Kiabus@users.noreply.github.com>
2023-01-03 10:48:48 +0100

    add find function

--- genshinImpact_firstHtml/connectionMongo.js
+++ genshinImpact_firstHtml/connectionMongo.js
@@ -1,4 +1,6 @@
-const DBUrl = "yourURLMongoDB";
+const { cursorTo } = require("readline");
+
+const DBUrl = "mongodb+srv://kiabus:LoScr1c1o@genshinimpact.jxjnt.mongodb.net/?retryWrites=true&w=majority";
 
 const MongoClient = require("mongodb").MongoClient;
 
@@ -8,9 +10,17 @@ mongodb.connect(function(err){
     if(err) console.log(err);
     else console.log("Connection successful!") /*on terminal write node connectionMongo.js to connect*/
 
-    //db and collection may exist on mondo db
+    //db and collection may exist on mongo db
     let characters = mongodb.db("genshin").collection("characters");
     let weapons = mongodb.db("genshin").collection("weapons");
+
+    //find character by name
+    const findCharacter = characters.find({name: "Fischl"});
+    findCharacter.forEach(console.dir);
+
+    //find weapon by name
+    const findWeapon = weapons.find({name: "Hunter's Bow"});
+    findWeapon.forEach(console.dir);
 
     //insert a record
     /*weapons.insertOne({name: "Messenger"})
