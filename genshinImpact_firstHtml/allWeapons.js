6a4a405c44d6aecdc80d3eaf9a25400a47ab6654
Kiabus <59321179+Kiabus@users.noreply.github.com>
2023-01-03 18:31:19 +0100

    Update allWeapons.js

--- genshinImpact_firstHtml/allWeapons.js
+++ genshinImpact_firstHtml/allWeapons.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:8080/api/weapons").then((data)=>{
+fetch("http://localhost:8080/genshin/weapons").then((data)=>{
     //console.log(data);
     return data.json(); //converted to object
 }).then((objectData) =>{
