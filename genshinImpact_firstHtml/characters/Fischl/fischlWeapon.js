d2feae119e93f8628081e1315547b131f59415f3
Kiabus <59321179+Kiabus@users.noreply.github.com>
2024-04-20 19:05:16 +0200

    update Fischl files

--- genshinImpact_firstHtml/characters/Fischl/fischlWeapon.js
+++ genshinImpact_firstHtml/characters/Fischl/fischlWeapon.js
@@ -1,4 +1,4 @@
-fetch("http://localhost:8080/api/characters/Fischl").then((data)=>{
+fetch("http://localhost:8080/api/weapons/Messenger").then((data)=>{
     //console.log(data);
     return data.json(); //converted to object
 }).then((objectData) =>{
@@ -8,14 +8,11 @@ fetch("http://localhost:8080/api/characters/Fischl").then((data)=>{
     objectData.map((values) => {
         tableData = `<tr>
         <td>${values.name}</td>
-        <td>${values.baseHP}</td>
+        <td>${values.type}</td>
         <td>${values.baseATK}</td>
-        <td>${values.baseDEF}</td>
-        <td>${values.eleMaster}</td>
-        <td>${values.maxStamina}</td>
+        <td>${values.bonusEffect}</td>
+        <td>${values.skill}</td>
         </tr>`;
     });
-    document.getElementById("table-char").innerHTML=tableData;    
+    document.getElementById("table-char").innerHTML=tableData;
 })
-
-
